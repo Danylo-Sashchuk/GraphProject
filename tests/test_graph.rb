0331cdf8caf8108734d281f3fff99bcb4b11afa2
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-10-06 17:22:38 -0400

    added tests

--- tests/test_graph.rb
+++ tests/test_graph.rb
@@ -2,6 +2,7 @@
 
 require 'minitest/autorun'
 require_relative '../model/graph'
+require_relative '../utils/util'
 
 class TestGraph < Minitest::Unit::TestCase
   attr_reader :graph
@@ -12,45 +13,90 @@ class TestGraph < Minitest::Unit::TestCase
 
   def test_initialize
     assert_equal([], @graph.get_nodes)
-    assert_nodes_size(0)
-    assert_edges_size(0)
+    assert_equal([], @graph.get_edges)
+    assert_node_and_edge_counts
   end
 
   def test_add_node1
     @graph.add_node(:a)
     assert_equal([:a], @graph.get_nodes)
-    assert_nodes_size(1)
+    assert_node_and_edge_counts({ nodes: 1 })
   end
 
   def test_add_node2
     @graph.add_node(:b)
     assert_equal([:b], @graph.get_nodes)
-    assert_nodes_size(1)
+    assert_node_and_edge_counts({ nodes: 1 })
   end
 
-  def test_add_node_existed_node
+  def test_add_node3
     @graph.add_node(:a)
-    @graph.add_node(:a)
-    assert_equal([:a], @graph.get_nodes)
-    assert_nodes_size(1)
+    @graph.add_node(:b)
+    @graph.add_node(:c)
+    assert(Util.arrays_equal_disregard_order(%i[a b c], @graph.get_nodes))
+    assert_node_and_edge_counts({ nodes: 3 })
   end
 
-  def test_add_node_as_node
+  def test_add_node_existed
+    @graph.add_node(:a)
+    assert_raises(ArgumentError) { @graph.add_node(:a) }
+    assert_node_and_edge_counts({ nodes: 1 })
+  end
+
+  def test_add_node_as_node1
     @graph.add_node(Node.new(:c))
     assert_equal([:c], @graph.get_nodes)
-    assert_nodes_size(1)
+    assert_node_and_edge_counts({ nodes: 1 })
+  end
+
+  def test_add_node_existed_as_node1
+    @graph.add_node(Node.new(:c))
+    assert_raises(ArgumentError) { @graph.add_node(Node.new(:c)) }
+    assert_node_and_edge_counts({ nodes: 1 })
+  end
+
+  def test_add_node_existed_as_node2
+    @graph.add_node(Node.new(:c))
+    assert_raises(ArgumentError) { @graph.add_node(:c) }
+    assert_node_and_edge_counts({ nodes: 1 })
   end
 
   def test_add_nodes1
     @graph.add_nodes(:a)
-    assert_equal([:a], @graph.get_nodes)
-    assert_nodes_size(1)
+    assert(Util.arrays_equal_disregard_order(%i[a], @graph.get_nodes))
+    assert_node_and_edge_counts({ nodes: 1 })
   end
 
   def test_add_nodes2
     @graph.add_nodes(:a, :b, :c)
-    assert_equal(%i[a b c], @graph.get_nodes)
-    assert_nodes_size(3)
+    assert(Util.arrays_equal_disregard_order(%i[a b c], @graph.get_nodes))
+    assert_node_and_edge_counts({ nodes: 3 })
+  end
+
+  def test_add_nodes3
+    @graph.add_nodes(:b, :a, :c)
+    assert(Util.arrays_equal_disregard_order(%i[a b c], @graph.get_nodes))
+    assert_node_and_edge_counts({ nodes: 3 })
+  end
+
+  def test_add_nodes4
+    @graph.add_nodes(:a, :b, Node.new(:c)) #TODO: can we add node? if can. fix in add_nodes
+    assert(Util.arrays_equal_disregard_order([], @graph.get_nodes))
+    assert_node_and_edge_counts({ nodes: 0 })
+  end
+
+  # TODO: Util.arrays_equal_disregard_order meke this method with assertion to convinient framework work
+
+  def test_add_nodes_existed
+    assert_raises(GraphException) { @graph.add_nodes(:a, :b, Node.new(:b)) }
+    assert(Util.arrays_equal_disregard_order([], @graph.get_nodes))
+    assert_node_and_edge_counts({ nodes: 0 })
+  end
+
+  def test_add_nodes_wrong_type
+    assert_raises(TypeError) { @graph.add_nodes(:a, Node.new(-1)) }
+    assert(Util.arrays_equal_disregard_order([], @graph.get_nodes))
+    assert_node_and_edge_counts({ nodes: 0 })
   end
 
   def test_add_edge
@@ -58,15 +104,22 @@ class TestGraph < Minitest::Unit::TestCase
     @graph.add_edge(:a, :b)
   end
 
+  private
+
   def add_many_nodes
     @graph.add_nodes(:a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l, :m, :n, :o)
   end
 
-  def assert_nodes_size(size)
+  def assert_node_and_edge_counts(args = {})
+    assert_node_counts(args[:nodes] || 0)
+    assert_edge_counts(args[:edges] || 0)
+  end
+
+  def assert_node_counts(size)
     assert_equal(size, @graph.nbr_nodes)
   end
 
-  def assert_edges_size(size)
+  def assert_edge_counts(size)
     assert_equal(size, @graph.nbr_edges)
   end
 end
