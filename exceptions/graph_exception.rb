9d9649824d5a342e750f74d90b5fb9f3b208377d
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-10-08 00:03:10 -0400

    exception

--- exceptions/graph_exception.rb
+++ exceptions/graph_exception.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 class GraphException < StandardError
-  def initialize(e)
-    super("GraphException occurred: #{e.class}: #{e.message}\n#{e.backtrace.join("\n")}")
+  attr_reader :underlying_exception
+
+  def initialize(msg = 'GraphException occurred.', exception = nil)
+    super(msg)
+    @underlying_exception = exception
+  end
+
+  def message_with_details
+    msg = "GraphException occurred:\n"
+    if underlying_exception
+      msg += "  Class: #{underlying_exception.class.name}\n"
+      msg += "  Message: #{underlying_exception.message}\n"
+      msg += "  Backtrace:\n#{underlying_exception.backtrace.join("\n")}\n"
+    else
+      msg += "  No underlying exception details available.\n"
+    end
+    msg
   end
 end
