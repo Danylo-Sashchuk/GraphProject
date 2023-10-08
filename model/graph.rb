7cd0c3d5d9cc8a57caabf2a8ee3c09d6573464ba
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-10-08 00:49:02 -0400

    add_edge in graph

--- model/graph.rb
+++ model/graph.rb
@@ -4,6 +4,7 @@ require_relative 'edge'
 require_relative '../exceptions/graph_exception'
 
 class Graph
+  # TODO: consider implementation of to_s through to_a
   attr_reader :nodes, :edges
 
   def initialize
@@ -18,6 +19,7 @@ class Graph
     @nodes << node
   end
 
+  # TODO: what about Array in parameter?
   def add_nodes(*nodes)
     added_nodes = Set.new
     begin
@@ -28,7 +30,7 @@ class Graph
       end
     rescue StandardError => e
       remove_added_nodes(added_nodes)
-      raise GraphException.new(e), "Exception occurred. Rollback the graph's nodes."
+      raise GraphException, "Exception occurred. Rollback the graph's nodes. \n#{e.message}"
     end
   end
 
@@ -41,7 +43,13 @@ class Graph
   end
 
   def add_edge(node_a, node_b)
-    edges << Edge.new(node_a, node_b)
+    node_a = Node.ensure_node(node_a)
+    node_b = Node.ensure_node(node_b)
+    unless @nodes.include?(node_a) && @nodes.include?(node_b)
+      raise GraphException, 'One of the nodes does not belong to the graph.'
+    end
+
+    @edges << Edge.new(node_a, node_b)
   end
 
   def get_edges
