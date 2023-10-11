5f033432f14f174699212846eefa9e42c5f39e89
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-10-10 20:13:16 -0400

    prevent duplicate lines

--- model/graph_renderer.rb
+++ model/graph_renderer.rb
@@ -3,48 +3,70 @@
 require 'victor'
 
 class GraphRenderer
-  def initialize(nodes, adjacency_list, center, radius)
-    @nodes = nodes
+  def initialize(center, radius, adjacency_list)
+    @nodes = adjacency_list.keys
     @adjacency_list = adjacency_list
     @center = center
     @radius = radius
+    @node_radius = 10
+    @visited_nodes = Set.new
   end
 
-  def setup_canvas(node_radius, padding)
-    canvas_width = 2 * (@radius + node_radius + padding)
-    canvas_height = 2 * (@radius + node_radius + padding)
+  def render(filename)
+    setup_canvas
 
-    @center[0] = [@center[0], @radius + node_radius + padding, canvas_width - @radius - node_radius - padding].sort[1]
-    @center[1] = [@center[1], @radius + node_radius + padding, canvas_height - @radius - node_radius - padding].sort[1]
+    @nodes.each do |node|
+      next if @visited_nodes.include?(node) # Skip already visited nodes to prevent duplicate lines
+
+      draw_node(node)
+      draw_edges(node)
+      @visited_nodes << node
+    end
+
+    @svg.save("output/#{filename}")
+  end
+
+  def setup_canvas
+    padding = 20
+
+    canvas_width = 2 * (@radius + @node_radius + padding)
+    canvas_height = 2 * (@radius + @node_radius + padding)
+
+    @center[0] = [@center[0], @radius + @node_radius + padding, canvas_width - @radius - @node_radius - padding].sort[1]
+    @center[1] = [@center[1], @radius + @node_radius + padding, canvas_height - @radius - @node_radius - padding].sort[1]
 
     @svg = Victor::SVG.new(viewBox: "0 0 #{canvas_width} #{canvas_height}", style: { background: 'white' })
   end
 
-  def render(filename)
-    angle_between_nodes = 360.0 / @nodes.length
-    padding = 20
-    node_radius = 10
+  def angle_between_nodes
+    360.0 / @nodes.length
+  end
 
-    setup_canvas(node_radius, padding)
+  def calculate_node_coordinates(angle)
+    node_x = @center[0] + @radius * Math.cos(angle * Math::PI / 180)
+    node_y = @center[1] + @radius * Math.sin(angle * Math::PI / 180)
+    [node_x, node_y]
+  end
 
-    @nodes.each_with_index do |node, index|
-      angle = index * angle_between_nodes
+  def draw_node(node)
+    angle = @nodes.index(node) * angle_between_nodes
+    node_x, node_y = calculate_node_coordinates(angle)
+    @svg.circle(cx: node_x, cy: node_y, r: @node_radius, fill: 'blue')
+  end
 
-      node_x = @center[0] + @radius * Math.cos(angle * Math::PI / 180)
-      node_y = @center[1] + @radius * Math.sin(angle * Math::PI / 180)
+  def draw_edges(node)
+    angle = @nodes.index(node) * angle_between_nodes
+    node_x, node_y = calculate_node_coordinates(angle)
 
-      @svg.circle(cx: node_x, cy: node_y, r: node_radius, fill: 'blue')
+    @visited_nodes.each do |visited_node|
+      next if visited_node == node
 
-      @adjacency_list[node].each do |adjacent_node|
-        adjacent_index = @nodes.index(adjacent_node)
-        adjacent_angle = adjacent_index * angle_between_nodes
-        adjacent_x = @center[0] + @radius * Math.cos(adjacent_angle * Math::PI / 180)
-        adjacent_y = @center[1] + @radius * Math.sin(adjacent_angle * Math::PI / 180)
+      next unless @adjacency_list[node].include?(visited_node)
 
-        @svg.line(x1: node_x, y1: node_y, x2: adjacent_x, y2: adjacent_y, stroke: 'red', 'stroke-width': 1)
-      end
+      visited_index = @nodes.index(visited_node)
+      adjacent_x, adjacent_y = calculate_node_coordinates(visited_index * angle_between_nodes)
+
+      @svg.line(x1: node_x, y1: node_y, x2: adjacent_x, y2: adjacent_y, stroke: 'red', 'stroke-width': 1)
     end
-
-    @svg.save("output/#{filename}")
   end
 end
--- test/expt.rb
+++ test/expt.rb
@@ -3,45 +3,6 @@ require_relative '../model/graph'
 require_relative '../utils/graph_utils'
 require 'victor'
 
-graph = GraphUtils.genCompleteGraph(10, 0.5)
-nodes = graph.nodes
-adjacency_list = graph.adjacency_list
-
-total_nodes = nodes.length
-angle_between_nodes = 360.0 / total_nodes
-
-# Create a new SVG document
-svg = Victor::SVG.new(width: 1000, height: 1000, viewBox: '0 0 1000 1000', style: { background: 'white' })
-
-# Define the center coordinates of the circle
-center_x = 500
-center_y = 500
-
-# Define the radius of the circle
-radius = 300
-
-nodes.each_with_index do |node, index|
-  # Calculate the angle for the current node
-  angle = index * angle_between_nodes
-
-  # Calculate the coordinates for the node position on the circle
-  node_x = center_x + radius * Math.cos(angle * Math::PI / 180)
-  node_y = center_y + radius * Math.sin(angle * Math::PI / 180)
-
-  # Create a circle for each node
-  svg.circle(cx: node_x, cy: node_y, r: 20, fill: 'blue')
-
-  # Loop through the adjacent nodes and draw edges
-  adjacency_list[node].each do |adjacent_node|
-    adjacent_index = nodes.index(adjacent_node)
-    adjacent_angle = adjacent_index * angle_between_nodes
-    adjacent_x = center_x + radius * Math.cos(adjacent_angle * Math::PI / 180)
-    adjacent_y = center_y + radius * Math.sin(adjacent_angle * Math::PI / 180)
-
-    # Draw an edge (line) between the current node and its adjacent node
-    svg.line(x1: node_x, y1: node_y, x2: adjacent_x, y2: adjacent_y, stroke: 'red', 'stroke-width': 1)
-  end
-
-end
-
-svg.save('graphs')
+graph = GraphUtils.genCompleteGraph(10)
+graph.render('comparable_graph123', [200, 200], 400)
+print graph.nbr_edges
